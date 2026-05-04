985495b0ecd2c9cbc3e623020b150727255caabf
depash nepal <depash2003@gmail.com>
2026-05-04 16:01:36 -0400

    made canvas and done with frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,65 +1,109 @@
+"use client"
 import Image from "next/image";
+import "./main.css"
+import React from 'react';
+import { useState, useRef } from 'react'
 
 export default function Home() {
+  const [brushColor] = useState('black')
+  const [brushSize] = useState(10);
+  const x = useRef<number>(0)
+  const y = useRef<number>(0)
+  const drawing = useRef<boolean>(false)
+
+  const onMouseDown = (e: React.MouseEvent) => {
+    const canvas = e.target as HTMLCanvasElement;
+
+    const bounding = canvas.getBoundingClientRect();
+
+    const ctx = canvas.getContext("2d")!;
+
+    x.current = e.clientX - bounding.left
+    y.current = e.clientY - bounding.top
+
+    const dotSize = brushSize / 2;
+    ctx.fillStyle = brushColor;
+
+    ctx.beginPath();
+    ctx.arc(x.current, y.current, dotSize, 0, Math.PI * 2, true);
+    ctx.fill();
+
+    drawing.current = true
+  }
+
+  const onMouseMove = (e: React.MouseEvent) => {
+    const canvas = e.target as HTMLCanvasElement;
+
+    const ctx = canvas.getContext("2d")!;
+
+    const bounding = canvas.getBoundingClientRect();
+
+    const newX = e.clientX - bounding.left
+    const newY = e.clientY - bounding.top
+
+    if (drawing.current) {
+      drawLine(ctx, x.current, y.current, newX, newY);
+      x.current = e.clientX - bounding.left
+      y.current = e.clientY - bounding.top
+    }
+  }
+
+  function drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+    context.beginPath();
+    context.strokeStyle = brushColor;
+    context.lineWidth = brushSize;
+    context.lineJoin = "round";
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.closePath();
+    context.stroke();
+  }
+
+  const onMouseUp = (e: React.MouseEvent) => {
+    drawing.current = false
+  }
+
+  const onMouseEnter = (e: React.MouseEvent) => {
+    const canvas = e.target as HTMLCanvasElement;
+
+    const bounding = canvas.getBoundingClientRect();
+
+    if (drawing.current) {
+      x.current = e.clientX - bounding.left
+      y.current = e.clientY - bounding.top
+    }
+  }
+
+  const clearButton = () => {
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d")!;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="page-container">
+      <canvas
+        className="canvas"
+        id={'canvas'}
+        width={1200}
+        height={650}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
+        onMouseEnter={onMouseEnter}
+        onMouseDown={onMouseDown}
+      />
+      <div className="btn-container">
+        <button className="guess-btn">
+          Guess
+        </button>
+        <button
+          className="guess-btn"
+          onClick={clearButton}
+        >
+          Clear
+        </button>
+      </div>
     </div>
   );
 }
