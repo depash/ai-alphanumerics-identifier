ea75cd9149487e42caf337ace8b1d697363069cf
depash nepal <depash2003@gmail.com>
2026-05-07 14:56:48 -0400

    adding steps to make ai from vegtable recognizer

--- backend/app/generate_models.py
+++ backend/app/generate_models.py
@@ -17,6 +17,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Step 1 make a tranfom that will be used to turn the images into vectores and it will resize,convert to tensor, then normalize
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Step 2 Pulls in the images and uses the prior tansform to turn the images into vectors
+
+
 mndataNum = MNIST('./images/numbers')
 mndataChar = MNIST('./images/letters')
 
@@ -25,3 +32,25 @@ images, labels = mndataChar.load_training()
 readable_labels = [chr(64 + l) for l in labels]
 
 index = random.randrange(0, len(images))
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Step 3 Sets up device to use the gpu if avalible and cuda then seeding if gpu is available so the resaults stay the same
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Step 4 Creating the model itself with what each layer does and how
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Step 5 pulls model down and puts it in the cpu/gpu and determine what algorithm to use
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Step 6 detirmine how many loops you run through and pulling data and putting it into the model to train
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Step 7 takes resaults from training and testing to see how accurate the model is using data the model hasn't seen
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Step 8 Saveing trained data
